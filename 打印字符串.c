8f0b13aa408decd2c973835bb78f9f6500bfaf46
B1ueTuT <3384549287@qq.com>
2024-09-22 15:20:23 +0800

    打印字符串.c

--- 打印字符串.c
+++ 打印字符串.c
@@ -2,8 +2,10 @@
 int main()
 {
 	char arr1[] = ("wonderful");
-    char arr2[] = { 's','w','e','e','t'};
+        char arr2[] = { 's','w','e','e','t'};
 	char arr3[] = { 'n','i','c','e','\0'};
+//arr2和arr3分别打印来体现\0的重要性
+//用双引号时自动添加\0而不显示
 	printf("%s\n", arr1);
 	printf("%s\n", arr2);
 	printf("%s\n", arr3);
