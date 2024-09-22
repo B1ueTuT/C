8d589c2ac11c83c6aabdd60a5479ffe5d7718779
B1ueTuT <3384549287@qq.com>
2024-09-22 15:15:30 +0800

    Add files via upload

---
+++ 打印字符串.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	char arr1[] = ("wonderful");
+    char arr2[] = { 's','w','e','e','t'};
+	char arr3[] = { 'n','i','c','e','\0'};
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	printf("%s\n", arr3);
+	return 0;
+}
